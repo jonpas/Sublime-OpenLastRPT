773ca459a604e58377982694152c109b68f651e6
jonpas <jonpas33@gmail.com>
2016-12-11 15:12:27 +0100

    Fix errors and ST2 compatibility

--- open_last_rpt.py
+++ open_last_rpt.py
@@ -3,6 +3,12 @@ import glob
 import sublime, sublime_plugin
 
 rpt_files = []
+ST3 = int(sublime.version()) >= 3000
+
+def move_to_eof(view):
+    if view.file_name() in rpt_files:
+        rpt_files.remove(view.file_name())
+        view.run_command("move_to", {"to": "eof"})
 
 class OpenLastRptCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -10,7 +16,6 @@ class OpenLastRptCommand(sublime_plugin.WindowCommand):
         base_folder = os.path.join(os.getenv("LOCALAPPDATA"), "Arma 3")
         newest_rpt_file = max(glob.iglob(os.path.join(base_folder, "*.rpt")), key=os.path.getctime)
         # Save file into list for EventListener
-        global rpt_files
         rpt_files.append(newest_rpt_file)
         # Open file
         self.window.open_file(newest_rpt_file)
@@ -18,7 +23,9 @@ class OpenLastRptCommand(sublime_plugin.WindowCommand):
 class EventListener(sublime_plugin.EventListener):
     # Called when a file is finished loading
     def on_load_async(self, view):
-        global rpt_files
-        if view.file_name() in rpt_files:
-            rpt_files.remove(view.file_name())
-            view.run_command("move_to", {"to": "eof"})
+        move_to_eof(view)
+
+    # ST2 Compatibility (has no on_load_async)
+    def on_load(self, view):
+        if not ST3:
+            move_to_eof(view)
